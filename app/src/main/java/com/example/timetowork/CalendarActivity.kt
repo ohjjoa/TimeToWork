e64a82c20c59883e2f992a7b084fc288f518c08e
TaeHyun <ohjjoa@naver.com>
2020-10-13 21:49:09 +0900

    calendarDayLayout

--- app/src/main/java/com/example/timetowork/CalendarActivity.kt
+++ app/src/main/java/com/example/timetowork/CalendarActivity.kt
@@ -1,12 +1,23 @@
 package com.example.timetowork
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.collections.HashSet
 
 class CalendarActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
 
+        val eventDays: HashSet<Date> = HashSet()
+        eventDays.add(Date())
+
+        //monthGap = pageCount - 1 - position
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.MONTH, calendar.get(Calendar.MONTH))
+        findViewById<CalendarView>(R.id.calendar_view).updateCalendar(eventDays,calendar)
+        //container.addView(view)
     }
 }
--- app/src/main/java/com/example/timetowork/CalendarView.kt
+++ app/src/main/java/com/example/timetowork/CalendarView.kt
@@ -6,24 +6,31 @@ import android.view.LayoutInflater
 import android.widget.GridView
 import android.widget.ImageView
 import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.calendar_layout.view.*
+import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
-import kotlin.collections.HashSet
 
 class CalendarView: LinearLayout {
     lateinit var header: LinearLayout
     lateinit var gridView : GridView
+    lateinit var imageCalendarPrev : ImageView
 
     constructor(context: Context) : this(context, null)
 
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0) {
         initControl(context, attrs!!) }
 
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
 
     private fun assignUiElements() {
         header = findViewById(R.id.layout_calendar_header)
         gridView = findViewById(R.id.grid_calendar)
+        imageCalendarPrev = image_calendar_prev
     }
 
     fun updateCalendar(events: HashSet<Date>, inputCalendar: Calendar) {
@@ -31,11 +38,18 @@ class CalendarView: LinearLayout {
 
         inputCalendar.set(Calendar.DAY_OF_YEAR, 1)
 
-        val monthBeginningCell = inputCalendar.get(Calendar.DAY_OF_WEEK) - 1
+        val monthBeginningCell = inputCalendar.get(Calendar.DAY_OF_WEEK)
 
         inputCalendar.add(Calendar.DAY_OF_MONTH, -monthBeginningCell)
 
-        // 그리드에 집어 넣을 cell들의 setup
+        // 타이틀 업데이트
+        val simpleDateFormat = SimpleDateFormat("EEEE,d MMM,yyyy")
+        val dateToday: List<String> = simpleDateFormat.format(inputCalendar.time).split(",")
+        text_date_display_day.text = dateToday[0]
+        text_date_display_date.text = dateToday[1]
+        text_date_display_year.text = dateToday[2]
+
+        // 그리드에 집어 넣을 cell 들의 setup
         while (cells.size < (Calendar.DAY_OF_MONTH) +
             inputCalendar.getActualMaximum(Calendar.DAY_OF_MONTH)) {
             cells.add(inputCalendar.time)
@@ -43,7 +57,12 @@ class CalendarView: LinearLayout {
         }
 
         // 그리드 업데이트
-        gridView.adapter = CalendarAdapter(context, cells, events, inputCalendar.get(Calendar.MONTH))
+        gridView.adapter = CalendarAdapter(
+            context,
+            cells,
+            events,
+            inputCalendar.get(Calendar.MONTH)
+        )
     }
 
    private fun initControl(context: Context, attrs: AttributeSet) {
@@ -51,5 +70,4 @@ class CalendarView: LinearLayout {
        inflater.inflate(R.layout.calendar_layout, this)
        assignUiElements()
    }
-
 }
